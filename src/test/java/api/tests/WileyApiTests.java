ce4fd550d9d81b547f22ab25aa6a127b19ddf346
Руслан Сибгатуллин <r.sibgatullin@smartcat.ai>
2021-09-13 22:40:59 +0300

    Test fixed

--- src/test/java/api/tests/WileyApiTests.java
+++ src/test/java/api/tests/WileyApiTests.java
@@ -1,9 +1,7 @@
 package api.tests;
 
 import io.restassured.response.Response;
-import org.junit.Before;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
